b28ad0811b69a5389793e2d74adb29e74f39287d
zqh <www.2221648485@qq.com>
2025-02-20 09:30:24 +0800

    2.20 refactor qt_sql.py

--- GUI/libs/qt_sql.py
+++ GUI/libs/qt_sql.py
@@ -1,96 +1,83 @@
-import pymysql
+from PySide6.QtSql import QSqlDatabase, QSqlQuery
 from REID.logger.log import get_logger
+import REID.config.model_cfgs as cfgs
 
-log = get_logger(__name__)
+log_info = get_logger(__name__)
 
 
-class MySQLDatabase:
-    def __init__(self, host='localhost', user='root', password='123456', charset='utf8mb4'):
-        self.host = host
-        self.user = user
-        self.password = password
-        self.charset = charset
-        self.connection = None
-
-    def connect(self, database=None):
-        try:
-            self.connection = pymysql.connect(
-                host=self.host,
-                user=self.user,
-                password=self.password,
-                database=database,
-                charset=self.charset,
-                cursorclass=pymysql.cursors.DictCursor
-            )
-            return True
-        except pymysql.Error as e:
-            log.error(f"Error connecting to database: {e}")
+def init_db(db_path, db_name):
+    try:
+        db = QSqlDatabase.addDatabase("QSQLITE")
+        db.setDatabaseName(db_path)
+        res = db.open()
+        if not db.open():
+            error_msg = db.lastError().text()
+            print(f"Error: Unable to open database - {error_msg}")
+            log_info.error(f"{db_path}_{db_name} Unable to open database: {error_msg}")
             return False
-
-    def create_database(self, db_name):
-        if self.connect():
-            with self.connection.cursor() as cursor:
-                cursor.execute(f"SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '{db_name}'")
-                result = cursor.fetchone()
-                if not result:
-                    cursor.execute(f"CREATE DATABASE {db_name}")
-                    log.info(f"Database {db_name} created successfully.")
-            self.connection.close()
-            return True
+        query = QSqlQuery()
+        query.exec_(
+            f"CREATE TABLE IF NOT EXISTS {db_name} (id integer primary key, name text, category varchar, box text, feat text, image varchar)")
+        return True
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        log_info.error(f"{db_path}_{db_name} An unexpected error occurred: {e}")
         return False
 
-    def create_table(self, db_name):
-        if self.connect(db_name):
-            with self.connection.cursor() as cursor:
-                sql = (f"CREATE TABLE IF NOT EXISTS {db_name} "
-                       f"(id INT PRIMARY KEY AUTO_INCREMENT, name TEXT, category VARCHAR(255), box TEXT, feat TEXT, image VARCHAR(255))")
-                cursor.execute(sql)
-            self.connection.commit()
-            self.connection.close()
-            return True
-        return False
 
-    def init_db(self, db_name):
-        if self.create_database(db_name):
-            return self.create_table(db_name)
-        return False
+def check(func, *args):
+    if not func(*args):
+        raise ValueError(func.__self__.lastError())
 
-    def load_sql_feat_info(self, db_name):
-        if self.connect(db_name):
-            try:
-                with self.connection.cursor() as cursor:
-                    sql = f"SELECT name, feat FROM {db_name}"
-                    cursor.execute(sql)
-                    results = cursor.fetchall()
-                    feat_list = []
-                    label_list = []
-                    for row in results:
-                        label_list.append(row['name'])
-                        feat_list.append(list(map(float, row['feat'].split(','))))
-                return feat_list, label_list
-            except pymysql.Error as e:
-                log.error(f"{db_name} query feature error: {e}")
-            finally:
-                self.connection.close()
-        return [], []
 
-    def add_register(self, db_name, name, category, box, feat, image):
-        if self.connect(db_name):
-            try:
-                with self.connection.cursor() as cursor:
-                    sql = f"INSERT INTO {db_name} (name, category, box, feat, image) VALUES (%s, %s, %s, %s, %s)"
-                    cursor.execute(sql, (name, category, box, feat, image))
-                self.connection.commit()
-                print("Query executed successfully")
-                return True
-            except pymysql.Error as e:
-                print(f"Error: {e}")
-                log.error(f"{db_name} insert data error: {e}")
-            finally:
-                self.connection.close()
+def load_sql_feat_info(db_path, db_name):
+    db = QSqlDatabase.addDatabase("QSQLITE")
+    db.setDatabaseName(db_path)
+    if not db.open():
+        print("Error: Unable to open database")
+        log_info.error("{}_{} Unable to open database.".format(db_path, db_name))
         return False
+    query = QSqlQuery()
+    query.exec("SELECT name, feat FROM {}".format(db_name))
+    feat_list = []
+    label_list = []
+    if not query.isActive():
+        log_info.error("{}_{} query feature error.".format(db_path, db_name))
+        print("Error:", query.lastError().text())
+    else:
+        results = []
+        while query.next():
+            # 通过列索引检索数据
+            data1 = query.value(0)  # 第一列的值
+            data2 = query.value(1)  # 第二列的值
+            label_list.append(data1)
+            feat_list.append(list(map(float, data2.split(','))))
+    return feat_list, label_list
+
+
+def _add_register(db_path, db_name, name, category, box, feat, image):
+    db = QSqlDatabase.addDatabase("QSQLITE")
+    db.setDatabaseName(db_path)
+    if not db.open():
+        print("Error: Unable to open database")
+        log_info.error("{}_{} Unable to open database.".format(db_path, db_name))
+        return False
+    q = QSqlQuery()
+    INSERT_BOOK_SQL = "insert into {}(name, category, box, feat, image) values(?, ?, ?, ?, ?)".format(db_name)
+    check(q.prepare, INSERT_BOOK_SQL)
+    q.addBindValue(name)
+    q.addBindValue(category)
+    q.addBindValue(box)
+    q.addBindValue(feat)
+    q.addBindValue(image)
+    q.exec()
+    if q.lastError().isValid():
+        # 查询执行失败，输出错误信息
+        print("Error:", q.lastError().text())
+    else:
+        # 查询执行成功
+        print("Query executed successfully")
 
 
 if __name__ == "__main__":
-    db = MySQLDatabase()
-    print(db.init_db("reid"))
+    init_db("reid.db", cfgs.DB_NAME)
